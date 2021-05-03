6aae461267177adba59a099ca44de3d2eb19f66d
Francis Adewale <71708756+FrancisAdewale@users.noreply.github.com>
2021-05-03 16:05:39 +0100

    set up view controller UI programmatically

--- TDDLeapYear/ViewController.swift
+++ TDDLeapYear/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var userInput = String()
+    
+    private let setUpUserInput : UITextField = {
+        
+        let input = UITextField()
+        input.translatesAutoresizingMaskIntoConstraints = false
+        input.clipsToBounds = true
+        input.layer.cornerRadius = 5.0
+        input.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        input.backgroundColor = .white
+//        input.addTarget(self, action: #selector(submittedLeapYear), for: .touchUpInside)
+        
+        return input
+        
+        
+    }()
+    
+    private let setUpButton: UIButton = {
+        
+        let btn = UIButton()
+        btn.clipsToBounds = true
+        btn.layer.cornerRadius = 5.0
+        btn.setTitle("Check", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(submittedLeapYear), for: .touchUpInside)
+
+        return btn
+        
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        view.addSubview(setUpUserInput)
+        view.addSubview(setUpButton)
+        setUpUserInput.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30).isActive = true
+        setUpUserInput.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30).isActive = true
+        setUpUserInput.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        setUpUserInput.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        setUpButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        setUpButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        setUpButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        setUpButton.centerYAnchor.constraint(equalTo: setUpUserInput.bottomAnchor, constant: 30).isActive = true
     }
-
+    
+    
+    @objc func submittedLeapYear() {
+        
+        userInput = setUpUserInput.text!
+        print(userInput)
+        
+        
+    }
 
 }
 
---
+++ TDDLeapYearTests/TDDLeapYearTests.swift
@@ -0,0 +1,34 @@
+//
+//  TDDLeapYearTests.swift
+//  TDDLeapYearTests
+//
+//  Created by Francis Adewale on 03/05/2021.
+//
+
+import XCTest
+@testable import TDDLeapYear
+
+class TDDLeapYearTests: XCTestCase {
+
+    override func setUp()  {
+    }
+
+    override func tearDown()  {
+    }
+
+    func test_readsUserInput() {
+        
+        //given
+        
+        
+        //when
+        
+        sut.submittedLeapYear()
+        
+        
+        //then
+        
+        
+    }
+
+}
